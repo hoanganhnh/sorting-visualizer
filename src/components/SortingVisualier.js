c05d3c8732283a96766294407d54c23950ba0937
Nguyễn Hoàng Anh <hoahoanganh20012001@gmail.com>
2021-10-26 18:54:42 +0700

    Demo shell sort

--- src/components/SortingVisualier.js
+++ src/components/SortingVisualier.js
@@ -18,7 +18,7 @@ const YELLOW_COLOR = 'yellow'
 const ORANGE_COLOR = 'orange'
 const PINK_COLOR = 'pink'
 
-const COUNT_RANGE = 10
+const COUNT_RANGE = 20
 
 function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min) + min)
@@ -363,6 +363,50 @@ function SortingVisualier() {
         await finishedSort(arr, 100)
     }
 
+    const shellSort = async () => {
+        const bars = document.querySelectorAll('.bar')
+
+        const size = bars.length
+
+        for (let i = Math.floor(size / 2); i > 0; i = Math.floor(i / 2)) {
+            for (let j = i; j < size; j++) {
+                const temp = getValueBar(bars, j)
+                let k
+                const temp1 = bars[j].style.height
+                const temp2 = getValueBar(bars, j)
+
+                await changeColorAnimation(bars, j, CORLOR_RUN, 100)
+
+                for (k = j; k >= i && getValueBar(bars, k - i) > temp; k -= i) {
+                    bars[k].style.height = bars[k - i].style.height
+
+                    bars[k].childNodes[0].innerText =
+                        bars[k - i].childNodes[0].innerText
+                    await changeColorAnimation(bars, k, ORANGE_COLOR, 100)
+                }
+                bars[j].style.backgroundColor = PINK_COLOR
+                // await changeColorAnimation(bars, j, PINK_COLOR, 100)
+                // await changeColorAnimation(bars, k, PINK_COLOR, 100)
+
+                bars[k].style.height = temp1
+                bars[k].childNodes[0].innerText = temp2
+
+                // bars[j].style.backgroundColor = 'rgb(0, 183, 255)'
+                // bars[k].style.backgroundColor = 'rgb(0, 183, 255)'
+
+                await Promise.all([
+                    changeColorAnimation(bars, j, PINK_COLOR, 50),
+                    changeColorAnimation(bars, k, PRIMARY_COLOR, 50),
+                ])
+            }
+        }
+        for (let x = 0; x < size; x++) {
+            // bars[x].style.backgroundColor = THIRST_COLOR
+            await changeColorAnimation(bars, x, THIRST_COLOR, 50)
+        }
+        await finishedSort(bars, 100)
+    }
+
     const resetArray = () => {
         const array = []
         for (let i = 0; i < COUNT_RANGE; i++) {
@@ -439,6 +483,9 @@ function SortingVisualier() {
             </button>
             <button type="button" onClick={insertionSort}>
                 Insertion sort
+            </button>
+            <button type="button" onClick={shellSort}>
+                Shell sort
             </button>
         </>
     )
